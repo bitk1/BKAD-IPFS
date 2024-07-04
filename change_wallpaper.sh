d6d040578ab0e7d196477fdeb76419c6ae77de4a
Tom J <tomj@Bitk-Air-2.lan>
2024-07-04 23:07:45 +0100

    Updates

--- change_wallpaper.sh
+++ change_wallpaper.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-#v03
-# Set the wallpaper image
-wallpaper_image="/home/bitk1/BKAD-IPFS/wallpaper.png"
+#v05
+# Create the script file
+echo "#!/bin/bash" >> /home/bitk1/change_wallpaper.sh
+echo "wallpaper_image=\"/home/bitk1/BKAD-IPFS/wallpaper.png\"" >> /home/bitk1/change_wallpaper.sh
+echo "pcmanfm --set-wallpaper \$wallpaper_image" >> /home/bitk1/change_wallpaper.sh
 
-# Set the wallpaper using pcmanfm
-pcmanfm --set-wallpaper $wallpaper_image
+# Make the script executable
+chmod +x /home/bitk1/change_wallpaper.sh
+
+# Add the script to crontab to run on startup
+crontab -l > tempcron
+echo "@reboot /home/bitk1/change_wallpaper.sh" >> tempcron
+crontab tempcron
+rm tempcron
