12a201c268f3750b763aed11680e1992163d0ad7
Tom J <tomj@Bitk-Air-2.lan>
2024-07-04 23:43:29 +0100

    Updates

--- shortcut.sh
+++ shortcut.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#v02
 # Create the desktop shortcut file
 echo "[Desktop Entry]" >> ~/Desktop/BKAD-IPFS.desktop
 echo "Type=Application" >> ~/Desktop/BKAD-IPFS.desktop
@@ -8,6 +8,9 @@ echo "Comment=BKAD-IPFS Web Interface" >> ~/Desktop/BKAD-IPFS.desktop
 echo "Exec=xdg-open http://127.0.0.1:5001/webui" >> ~/Desktop/BKAD-IPFS.desktop
 echo "Icon=/Users/tomj/Downloads/github/BKAD-IPFS/bk_circle_300.png" >> ~/Desktop/BKAD-IPFS.desktop
 echo "Terminal=false" >> ~/Desktop/BKAD-IPFS.desktop
+
+# Remove the wastebasket icon
+rm ~/Desktop/Trash.desktop
 
 # Make the file executable
 chmod +x ~/Desktop/BKAD-IPFS.desktop
