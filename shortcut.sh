d709f2a96bab7cde4d3ef439ecf7535552bd9c20
Tom J <tomj@Bitk-Air-2.lan>
2024-07-04 23:37:04 +0100

    Updates

---
+++ shortcut.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create the desktop shortcut file
+echo "[Desktop Entry]" >> ~/Desktop/BKAD-IPFS.desktop
+echo "Type=Application" >> ~/Desktop/BKAD-IPFS.desktop
+echo "Name=BKAD-IPFS" >> ~/Desktop/BKAD-IPFS.desktop
+echo "Comment=BKAD-IPFS Web Interface" >> ~/Desktop/BKAD-IPFS.desktop
+echo "Exec=xdg-open http://127.0.0.1:5001/webui" >> ~/Desktop/BKAD-IPFS.desktop
+echo "Icon=/Users/tomj/Downloads/github/BKAD-IPFS/bk_circle_300.png" >> ~/Desktop/BKAD-IPFS.desktop
+echo "Terminal=false" >> ~/Desktop/BKAD-IPFS.desktop
+
+# Make the file executable
+chmod +x ~/Desktop/BKAD-IPFS.desktop
