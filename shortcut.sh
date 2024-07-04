1ccd44473384926d7055925672aa4bd00d020032
Tom J <tomj@Bitk-Air-2.lan>
2024-07-04 23:45:26 +0100

    Updates

--- shortcut.sh
+++ shortcut.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#v02
+#v05
 # Create the desktop shortcut file
 echo "[Desktop Entry]" >> ~/Desktop/BKAD-IPFS.desktop
 echo "Type=Application" >> ~/Desktop/BKAD-IPFS.desktop
 echo "Name=BKAD-IPFS" >> ~/Desktop/BKAD-IPFS.desktop
 echo "Comment=BKAD-IPFS Web Interface" >> ~/Desktop/BKAD-IPFS.desktop
 echo "Exec=xdg-open http://127.0.0.1:5001/webui" >> ~/Desktop/BKAD-IPFS.desktop
-echo "Icon=/Users/tomj/Downloads/github/BKAD-IPFS/bk_circle_300.png" >> ~/Desktop/BKAD-IPFS.desktop
+echo "Icon=~/BKAD-IPFS/bk_circle_300.png" >> ~/Desktop/BKAD-IPFS.desktop
 echo "Terminal=false" >> ~/Desktop/BKAD-IPFS.desktop
 
 # Remove the wastebasket icon
