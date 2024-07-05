970be927490df903e4f81a59de438aa1cd7a990c
Tom J <tomj@Bitk-Air-2.lan>
2024-07-05 01:37:01 +0100

    Updates

--- desktop.sh
+++ desktop.sh
@@ -3,22 +3,15 @@
 # Set HOME variable explicitly
 HOME=/home/bitk1
 
-# Create lxsession and LXDE directories if they do not exist
-if [ ! -d $HOME/.config/lxsession ]; then
-    mkdir -p $HOME/.config/lxsession
-fi
-if [ ! -d $HOME/.config/lxsession/LXDE ]; then
-    mkdir -p $HOME/.config/lxsession/LXDE
+# Create lxpanel directory if it does not exist
+if [ ! -d $HOME/.config/lxpanel ]; then
+    mkdir -p $HOME/.config/lxpanel
 fi
 
-# Create desktop.conf file if it does not exist
-if [ ! -f $HOME/.config/lxsession/LXDE/desktop.conf ]; then
-    touch $HOME/.config/lxsession/LXDE/desktop.conf
+# Create panels file if it does not exist
+if [ ! -f $HOME/.config/lxpanel/LXDE/panels ]; then
+    touch $HOME/.config/lxpanel/LXDE/panels
 fi
 
-# Set desktop to dark mode
-echo "[GTK]" >> $HOME/.config/lxsession/LXDE/desktop.conf
-echo "theme=Dark" >> $HOME/.config/lxsession/LXDE/desktop.conf
-
-# Hide Wastebasket icon
-echo "show_trash=0" >> $HOME/.config/pcmanfm/LXDE-pi/desktop-items-0.conf
+# Hide taskbar
+echo "taskbar.hide=1" >> $HOME/.config/lxpanel/LXDE/panels
