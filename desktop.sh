8748100242575cdf0cc512a089b38f7b6530e9f7
Tom J <tomj@Bitk-Air-2.lan>
2024-07-05 01:41:32 +0100

    Updates

--- desktop.sh
+++ desktop.sh
@@ -8,6 +8,11 @@ if [ ! -d $HOME/.config/lxpanel ]; then
     mkdir -p $HOME/.config/lxpanel
 fi
 
+# Create LXDE directory if it does not exist
+if [ ! -d $HOME/.config/lxpanel/LXDE ]; then
+    mkdir -p $HOME/.config/lxpanel/LXDE
+fi
+
 # Create panels file if it does not exist
 if [ ! -f $HOME/.config/lxpanel/LXDE/panels ]; then
     touch $HOME/.config/lxpanel/LXDE/panels
---
+++ rm_wastebasket.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+# Hide Wastebasket icon
+echo "show_trash=0" >> ~/.config/pcmanfm/LXDE-pi/desktop-items-0.conf
