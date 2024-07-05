de60d675c69fe756ba1c2602e60a68bab533919b
Tom J <tomj@Bitk-Air-2.lan>
2024-07-05 05:05:10 +0100

    Updates

--- desktop.sh
+++ desktop.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
 
-# Set HOME variable explicitly
-HOME=/home/bitk1
-
-# Create lxpanel directory if it does not exist
-if [ ! -d $HOME/.config/lxpanel ]; then
-    mkdir -p $HOME/.config/lxpanel
-fi
-
-# Create LXDE directory if it does not exist
-if [ ! -d $HOME/.config/lxpanel/LXDE ]; then
-    mkdir -p $HOME/.config/lxpanel/LXDE
-fi
-
-# Create panels file if it does not exist
-if [ ! -f $HOME/.config/lxpanel/LXDE/panels ]; then
-    touch $HOME/.config/lxpanel/LXDE/panels
-fi
-
-# Change taskbar color to black
-echo "bg.color=#000000" >> $HOME/.config/lxpanel/LXDE/panels
+# Change the taskbar color to black
+sed -i 's/tintcolor=#000000/tintcolor=#000000/g' /etc/xdg/lxpanel/LXDE-pi/panels/panel
