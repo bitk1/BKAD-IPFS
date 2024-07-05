744e1a660faa9571dfae078e05924fba2ab3409b
Tom J <tomj@Bitk-Air-2.lan>
2024-07-05 01:26:14 +0100

    Updates

--- desktop.sh
+++ desktop.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-#v08
+
 # Set HOME variable explicitly
 HOME=/home/bitk1
+
+# Create desktop.conf file if it does not exist
+if [ ! -f $HOME/.config/lxsession/LXDE/desktop.conf ]; then
+    touch $HOME/.config/lxsession/LXDE/desktop.conf
+fi
 
 # Set desktop to dark mode
 echo "[GTK]" >> $HOME/.config/lxsession/LXDE/desktop.conf
