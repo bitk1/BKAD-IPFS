303858af6331d6276c4d132ed6c127fd86ea9b63
Tom J <tomj@Bitk-Air-2.lan>
2024-07-13 13:20:56 +0100

    Updates

--- 034setup_ipfs.sh
+++ 034setup_ipfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script sets up IPFS on a Raspberry Pi and ensures network accessibility and SSH access is preserved.
+# This script sets up IPFS on a Raspberry Pi, including adjusting CORS settings to ensure the WebUI can connect.
 # It is assumed that this script is run with root privileges.
 
 echo "Starting IPFS setup..."
@@ -23,6 +23,10 @@ fi
 # Set IPFS to listen on all network interfaces
 sudo -u bitk1 -H bash -c "ipfs config --json Addresses.API '\"/ip4/0.0.0.0/tcp/5001\"'"
 sudo -u bitk1 -H bash -c "ipfs config --json Addresses.Gateway '\"/ip4/0.0.0.0/tcp/8080\"'"
+
+# Adjust CORS settings
+sudo -u bitk1 -H bash -c "ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '[\"http://192.168.1.103:5001\", \"http://localhost:3000\", \"http://127.0.0.1:5001\", \"https://webui.ipfs.io\"]'"
+sudo -u bitk1 -H bash -c "ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '[\"PUT\", \"GET\", \"POST\"]'"
 
 # Create Systemd Service for IPFS
 echo "Configuring IPFS service..."
