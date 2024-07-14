2d6b71a8690b5620e8a8e08cc37d79b1d084b5d5
Tom J <tomj@Bitk-Air-2.lan>
2024-07-14 12:44:49 +0100

    Updates

--- shortcut.sh
+++ shortcut.sh
@@ -4,7 +4,8 @@ echo "[Desktop Entry]" > /home/bitk1/Desktop/BKAD-IPFS.desktop
 echo "Type=Application" >> /home/bitk1/Desktop/BKAD-IPFS.desktop
 echo "Name=BKAD-IPFS" >> /home/bitk1/Desktop/BKAD-IPFS.desktop
 echo "Comment=BKAD-IPFS Web Interface" >> /home/bitk1/Desktop/BKAD-IPFS.desktop
-echo "Exec=chromium-browser --start-fullscreen http://127.0.0.1:5001/webui" >> /home/bitk1/Desktop/BKAD-IPFS.desktop
+# Modify below line to open window maximized instead of fullscreen
+echo "Exec=chromium-browser --start-maximized http://127.0.0.1:5001/webui" >> /home/bitk1/Desktop/BKAD-IPFS.desktop
 echo "Icon=/home/bitk1/BKAD-IPFS/bk_circle_300.png" >> /home/bitk1/Desktop/BKAD-IPFS.desktop
 echo "Terminal=false" >> /home/bitk1/Desktop/BKAD-IPFS.desktop
 
